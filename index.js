3c82d1215db1226ccbc4433bf378506982779c33
Jose Eduardo Perdomo <jepz20@gmail.com>
2016-09-08 14:59:01 -0600

    save files in json folder

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.signin(
 	(err, data) =>  {
 
 		client.list('CourseInventory').get((err, courses) => {
-			fs.writeFile(`courses.json`, JSON.stringify(courses))
+			fs.writeFile(`jsons/courses.json`, JSON.stringify(courses))
 		})
 
 		client.list('ModuleInventory').get((err, modules) => {
@@ -19,21 +19,21 @@ client.signin(
 				courses_uri = uri.slice(uri.indexOf('.svc/') + 5)
 				client.list(courses_uri).get((err2, cour) => {
 					module.CoursesDetail = cour.results
-					fs.writeFile(`modules.json`, JSON.stringify(modules))
+					fs.writeFile(`jsons/modules.json`, JSON.stringify(modules))
 				})
 			})
 		});
 
 		client.list('UnitInventory').get((err, units) => {
-			fs.writeFile(`units.json`, JSON.stringify(units))
+			fs.writeFile(`jsons/units.json`, JSON.stringify(units))
 		});
 
 		client.list('ActivityInventory').get((err, activities) => {
-			fs.writeFile(`activities.json`, JSON.stringify(activities))
+			fs.writeFile(`jsons/activities.json`, JSON.stringify(activities))
 		});
 
 		client.list('ResourcesInventory').get((err, resources) => {
-			fs.writeFile(`resources.json`, JSON.stringify(resources))
+			fs.writeFile(`jsons/resources.json`, JSON.stringify(resources))
 		});
 		// client.list('CourseInventory(1)/Attachments').get((err, data) => {
 		// 	fs.writeFile("ci2.json", JSON.stringify(data))
